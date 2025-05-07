45683a8aacc525cf0319e6bad0dd002982f5bb8a
haonan <18503111452@163.com>
2025-05-07 22:47:48 +0800

    修改布隆过滤器自动装配类

--- damai-redisson-framework/damai-redisson-service-framework/damai-bloom-filter-framework/src/main/java/com/damai/config/BloomFilterAutoConfiguration.java
+++ damai-redisson-framework/damai-redisson-service-framework/damai-bloom-filter-framework/src/main/java/com/damai/config/BloomFilterAutoConfiguration.java
@@ -2,9 +2,12 @@ package com.damai.config;
 
 
 import com.damai.handler.BloomFilterHandler;
+import com.damai.util.SpringUtil;
 import org.redisson.api.RedissonClient;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.DependsOn;
 
 /**
  * @author: haonan
@@ -17,6 +20,7 @@ public class BloomFilterAutoConfiguration {
      * 布隆过滤器
      */
     @Bean
+    @DependsOn("springUtil")
     public BloomFilterHandler rBloomFilterUtil(RedissonClient redissonClient, BloomFilterProperties bloomFilterProperties) {
         return new BloomFilterHandler(redissonClient, bloomFilterProperties);
     }
