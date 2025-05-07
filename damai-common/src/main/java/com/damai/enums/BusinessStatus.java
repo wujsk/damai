a2bee79be44d2147d5c06958a8dc3dd7594f92ae
haonan <18503111452@163.com>
2025-05-07 22:45:40 +0800

    新增枚举类

---
+++ damai-common/src/main/java/com/damai/enums/BusinessStatus.java
@@ -0,0 +1,51 @@
+package com.damai.enums;
+
+
+/**
+ * @author: haonan
+ * @description: 通用状态枚举
+ */
+public enum BusinessStatus {
+
+    /**
+     * 通用状态枚举
+     * */
+    YES(1,"是"),
+    NO(0,"否")
+            ;
+
+    private Integer code;
+
+    private String msg;
+
+    BusinessStatus(Integer code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public String getMsg() {
+        return this.msg == null ? "" : this.msg;
+    }
+
+    public static String getMsg(Integer code) {
+        for (BusinessStatus re : BusinessStatus.values()) {
+            if (re.code.intValue() == code.intValue()) {
+                return re.msg;
+            }
+        }
+        return "";
+    }
+
+    public static BusinessStatus getRc(Integer code) {
+        for (BusinessStatus re : BusinessStatus.values()) {
+            if (re.code.intValue() == code.intValue()) {
+                return re;
+            }
+        }
+        return null;
+    }
+}
---
+++ damai-common/src/main/java/com/damai/enums/Status.java
@@ -0,0 +1,51 @@
+package com.damai.enums;
+
+
+/**
+ * @author: haonan
+ * @description: 状态
+ */
+public enum Status {
+
+    /**
+     * 基础状态
+     * */
+    RUN(1,"正常"),
+    STOP(0,"禁用")
+    ;
+
+    private Integer code;
+
+    private String msg;
+
+    Status(Integer code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public String getMsg() {
+        return this.msg == null ? "" : this.msg;
+    }
+
+    public static String getMsg(Integer code) {
+        for (Status re : Status.values()) {
+            if (re.code.intValue() == code.intValue()) {
+                return re.msg;
+            }
+        }
+        return "";
+    }
+
+    public static Status getRc(Integer code) {
+        for (Status re : Status.values()) {
+            if (re.code.intValue() == code.intValue()) {
+                return re;
+            }
+        }
+        return null;
+    }
+}
