0115cee5647604b6c798f1fb2b9265ed5ab083d6
haonan <18503111452@163.com>
2025-05-07 22:49:25 +0800

    管理锁\缓存时长

---
+++ damai-redisson-framework/damai-redisson-service-framework/damai-redisson-common-framework/src/main/java/com/damai/constant/RedisTimeManager.java
@@ -0,0 +1,15 @@
+package com.damai.constant;
+
+
+/**
+ * @author: haonan
+ * @description: 锁\缓存时长
+ */
+public class RedisTimeManager {
+
+    // 3天
+    public static final int TICKET_USER_LIST_CACHE_TIME = 3;
+
+    // 5秒
+    public static final int TICKET_USER_LIST_LOCK_TIME = 2;
+}
